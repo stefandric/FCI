7ee72e58684812314cafcfd8bd5b8db154f1f67a
Stefan Andric <stefandric@hotmail.rs>
2016-10-05 17:41:18 +0200

    Initial commit of project with pods

---
+++ FCI/FCI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FCI
+//
+//  Created by Stefan Andric on 10/5/16.
+//  Copyright © 2016 stefandric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
